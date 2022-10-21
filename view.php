57a8130278259721eeaea664079bc3c30b36562c
Sang Tran <100749058+stran2312@users.noreply.github.com>
2022-10-21 10:51:00 -0500

    Add files via upload

--- view.php
+++ view.php
@@ -1,35 +1,32 @@
 <link href="assets/css/bootstrap.css" rel="stylesheet" />
-<link href="assets/css/bootstrap-fileupload.min.css" rel="stylesheet" />
 <!-- JQUERY SCRIPTS -->
 <script src="assets/js/jquery-1.12.4.js"></script>
 <!-- BOOTSTRAP SCRIPTS -->
 <script src="assets/js/bootstrap.js"></script>
-<script src="assets/js/bootstrap-fileupload.js"></script>
 <?php
 include("functions.php");
-$dblink=db_connect("document");
+$dblink=db_connect("docStorage");
+$autoid=$_REQUEST['fid'];
 echo '<div id="page-inner">';
 echo '<h1 class="page-head-line">View Files on DB</h1>';
 echo '<div class="panel-body">';
-$sql="Select * from `doc` where `status`='active'";
+$sql="Select * from `documents` where `auto_id`='$autoid'";
 $result=$dblink->query($sql) or
 	die("Something went wrong with $sql<br>".$dblink->error);
-while ($data=$result->fetch_array(MYSQLI_ASSOC))
+$data=$result->fetch_array(MYSQLI_ASSOC);
+if ($data['path']!=NULL)
+	echo '<p>File: <a href="uploads/'.$data['name'].'" target="_blank">'.$data['name'].'</a></p>';
+else
 {
-	if ($data['path']!=NULL)
-		echo '<p>File: <a href="uploads/'.$data['file_name'].'">'.$data['file_name'].'</a></p>';
+	$content=$data['content'];
+	$fname=date("Y-m-d_H:i:s")."-userid-file.pdf";
+	if (!($fp=fopen("/var/www/html/uploads/$fname","w")))
+		echo "<p>File could not be loaded at this time</p>";
 	else
 	{
-		$content=$data['content'];
-		$fname=date("Y-m-d_H:i:s")."-userid-file.pdf";
-		if (!($fp=fopen("/var/www/html/uploads/$fname","w")))
-			echo "<p>File could not be loaded at this time</p>";
-		else
-		{
-			fwrite($fp,$content);
-			fclose($fp);
-			echo '<p>File: <a href="uploads/'.$fname.'">'.$data['file_name'].'</a></p>';
-		}
+		fwrite($fp,$content);
+		fclose($fp);
+		echo '<p>File: <a href="uploads/'.$fname.'" target="_blank">'.$data['name'].'</a></p>';
 	}
 }
 echo '</div>';//end panel-body
